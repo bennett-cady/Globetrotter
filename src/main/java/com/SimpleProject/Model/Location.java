ff45760773568ab09e8ba52a4de83acaa8d02295
bennett-cady <bennett.cady14@gmail.com>
2025-06-17 18:52:55 -0600

    Fixed issue with hibernate dependency, the application is now able to run and access the database

--- src/main/java/com/SimpleProject/Model/Location.java
+++ src/main/java/com/SimpleProject/Model/Location.java
@@ -2,13 +2,16 @@ package com.SimpleProject.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="gen_location")
 public class Location {
 
-	
+	@Id
 	@Column(name="id")
 	public int id;
 	
